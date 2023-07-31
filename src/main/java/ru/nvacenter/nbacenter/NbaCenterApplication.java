3ff1c180e6709e7281aef0440264a5223b122ab3
glebpad <glebpad@yandex.ru>
2023-07-31 17:42:24 +0300

    add GreenBankCard impl

--- src/main/java/ru/nvacenter/nbacenter/NbaCenterApplication.java
+++ src/main/java/ru/nvacenter/nbacenter/NbaCenterApplication.java
@@ -1,14 +1,20 @@
 package ru.nvacenter.nbacenter;
 
-import ru.nvacenter.nbacenter.entity.BankCard;
-import ru.nvacenter.nbacenter.entity.CreditCard;
-import ru.nvacenter.nbacenter.entity.DebitCard;
+import ru.nvacenter.nbacenter.entity.GreenBankCard;
 
 import java.math.BigDecimal;
 
 public class NbaCenterApplication {
 
     public static void main(String[] args) {
+
+        GreenBankCard greenBankCard = new GreenBankCard(new BigDecimal(0), new BigDecimal(10000));
+
+        System.out.println(greenBankCard.getAllFunds());
+
+        greenBankCard.pay(new BigDecimal(1000));
+
+        System.out.println(greenBankCard.getAllFunds());
 
 
     }
--- src/main/java/ru/nvacenter/nbacenter/entity/GreenBankCard.java
+++ src/main/java/ru/nvacenter/nbacenter/entity/GreenBankCard.java
@@ -10,6 +10,19 @@ public class GreenBankCard extends CreditCard implements Cashbackable {
         super(balance, creditLimit);
     }
 
+    @Override
+    public Boolean pay(BigDecimal amountToDeducted) {
+        Boolean transactionResult = super.pay(amountToDeducted);
 
+        if (transactionResult) {
+            BigDecimal cashback = amountToDeducted.multiply(new BigDecimal("0.01"));
+            giveCashback(cashback);
+            System.out.println(String.format("You've earned cashback! It's %s", cashback));
+            return true;
+        } else return false;
+    }
 
+    private void giveCashback(BigDecimal cashback) {
+        super.addMoney(cashback);
+    }
 }
